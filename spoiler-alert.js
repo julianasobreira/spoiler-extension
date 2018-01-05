2e555754a2b1483c1be14a8642176acc7bbb72b1
Juliana Bacelar <juliana.univasf@gmail.com>
2018-01-05 06:14:06 -0200

    Test prettier

--- spoiler-alert.js
+++ spoiler-alert.js
@@ -1,5 +1,4 @@
-console.log('spoiler-alert')
-
+console.log("spoiler-alert");
 
 function getText(range) {
   var hasStarted = false;
@@ -7,47 +6,65 @@ function getText(range) {
   function recursor(n) {
     if (n.nodeType !== 3) {
       if (n.childNodes)
-        for (var i = 0; i < n.childNodes.length; ++i)
-          recursor(n.childNodes[i]);
+        for (var i = 0; i < n.childNodes.length; ++i) recursor(n.childNodes[i]);
     } else {
-      if(n === range.startContainer) {
+      if (n === range.startContainer) {
         hasStarted = true;
-        n.nodeValue = n.nodeValue.slice(0, range.startOffset) + 
-                      n.nodeValue.slice(range.startOffset, n.nodeValue.length).toUpperCase();
-      } else if(n === range.endContainer) {
+        n.nodeValue =
+          n.nodeValue.slice(0, range.startOffset) +
+          n.nodeValue
+            .slice(range.startOffset, n.nodeValue.length)
+            .toUpperCase();
+      } else if (n === range.endContainer) {
         hasEnded = true;
-        n.nodeValue = n.nodeValue.slice(0, range.endOffset).toUpperCase() + 
-                      n.nodeValue.slice(range.endOffset, n.nodeValue.length);
+        n.nodeValue =
+          n.nodeValue.slice(0, range.endOffset).toUpperCase() +
+          n.nodeValue.slice(range.endOffset, n.nodeValue.length);
       } else {
-        if(hasStarted && !hasEnded) {
+        if (hasStarted && !hasEnded) {
           n.nodeValue = n.nodeValue.toUpperCase();
-        } 
+        }
       }
     }
   }
 
   if (range.startContainer === range.endContainer) {
-    range.startContainer.nodeValue = range.startContainer.nodeValue.slice(0, range.startOffset) + 
-                                     range.startContainer.nodeValue.slice(range.startOffset, range.endOffset).toUpperCase() + 
-                                     range.startContainer.nodeValue.slice(range.endOffset, range.startContainer.nodeValue.length);
+    range.startContainer.nodeValue =
+      range.startContainer.nodeValue.slice(0, range.startOffset) +
+      range.startContainer.nodeValue
+        .slice(range.startOffset, range.endOffset)
+        .toUpperCase() +
+      range.startContainer.nodeValue.slice(
+        range.endOffset,
+        range.startContainer.nodeValue.length
+      );
   } else {
     recursor(range.commonAncestorContainer);
   }
 }
 
-function replaceTxt () {
+function replaceTxt() {
   var activeElement = document.activeElement;
   var selection = window.getSelection();
-  if (selection.toString() != '') {
+  if (selection.toString() != "") {
     if (activeElement.isContentEditable) {
       var range = selection.getRangeAt(0);
       getText(range);
     }
 
-    if (activeElement.tagName === 'TEXTAREA' || activeElement.tagName === 'INPUT') {
-      activeElement.value = activeElement.value.slice(0, activeElement.selectionStart) + 
-                            activeElement.value.slice(activeElement.selectionStart, activeElement.selectionEnd).toUpperCase() + 
-                            activeElement.value.slice(activeElement.selectionEnd, activeElement.value.length);
+    if (
+      activeElement.tagName === "TEXTAREA" ||
+      activeElement.tagName === "INPUT"
+    ) {
+      activeElement.value =
+        activeElement.value.slice(0, activeElement.selectionStart) +
+        activeElement.value
+          .slice(activeElement.selectionStart, activeElement.selectionEnd)
+          .toUpperCase() +
+        activeElement.value.slice(
+          activeElement.selectionEnd,
+          activeElement.value.length
+        );
     }
 
     selection.removeAllRanges();
